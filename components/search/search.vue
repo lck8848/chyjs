b74972f8bd1abdbea59e3d4bc456ca0d3a044b8f
shuoge <971749387@qq.com>
2020-10-15 21:03:48 +0800

    搜索框

---
+++ components/search/search.vue
@@ -0,0 +1,93 @@
+<template>
+	<view>
+		<view class="tui-searchbox">
+			<view class="tui-rolling-search">
+				<icon type="search" :size='13' color='#999'></icon>
+				<swiper vertical autoplay circular interval="8000" class="tui-swiper">
+					<swiper-item v-for="(item,index) in hotSearch" :key="index" class="tui-swiper-item" @tap="search">
+						<view class="tui-hot-item">大家正在搜：{{item}}</view>
+					</swiper-item>
+				</swiper>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				hotSearch: [
+					"螺蛳粉",
+					"榴莲千层",
+					"肉松海苔卷"
+				],
+			};
+		},
+		methods:{
+			search: function() {
+				uni.navigateTo({
+					url: '/pages/search/search' 
+				})
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+.tui-searchbox {
+		padding: 16rpx 20rpx;
+		box-sizing: border-box;
+		background-color: #F2F2F2;
+	}
+
+	.tui-rolling-search {
+		width: 100%;
+		height: 60rpx;
+		// border-radius: 35rpx;s
+		padding: 0 40rpx 0 30rpx;
+		box-sizing: border-box;
+		background: #fff;
+		display: flex;
+		align-items: center;
+		flex-wrap: nowrap;
+		color: #999;
+	}
+
+	.tui-swiper {
+		font-size: 26rpx;
+		height: 60rpx;
+		flex: 1;
+		padding-left: 12rpx;
+	}
+
+	.tui-swiper-item {
+		display: flex;
+		align-items: center;
+	}
+
+	.tui-hot-item {
+		line-height: 26rpx;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+
+	.tui-banner-swiper {
+		width: 100%;
+		height: 300rpx;
+		position: relative;
+	}
+
+	.tui-slide-image {
+		width: 100%;
+		height: 300rpx;
+		display: block;
+	}
+	
+	.tui-icon-delete {
+		padding: 10rpx;
+	}
+	
+
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,28 +1,35 @@
 <template>
 	<view class="container">
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
+		<view class="search-container" @click="Tosearch">
+			<search  ref="search"></search>
+		</view>
+		
+		
+		
 	</view>
 </template>
 
 <script>
+	import search from "../../components/search/search"
 	export default {
+		components:{
+			search
+		},
 		data() {
 			return {
 				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
 			}
 		},
 		methods: {
-
+			Tosearch(){
+				this.$refs.search.search()
+			}
 		}
 	}
 </script>
 
 <style>
 	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
+		
 	}
 </style>
