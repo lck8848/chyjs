9811e87e45a9ee67c318ec35a2a85398b4d502a4
yao <54076703@qq.com>
2020-10-15 21:55:46 +0800

    1112

--- pages/activity/vip/vip.vue
+++ pages/activity/vip/vip.vue
@@ -1,8 +1,6 @@
 <template>
-	<view class="container">
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
+	<view class="vip-container">
+
 	</view>
 </template>
 
@@ -10,7 +8,6 @@
 	export default {
 		data() {
 			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
 			}
 		},
 		methods: {
@@ -20,7 +17,7 @@
 </script>
 
 <style>
-	.container {
+	.vip.container {
 		padding: 20px;
 		font-size: 14px;
 		line-height: 24px;
