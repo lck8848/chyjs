d2d0f4a7bca68d3dbab76376af981a8132c43b2c
yao <54076703@qq.com>
2020-10-15 22:11:52 +0800

    img

--- pages/activity/vip/vip.vue
+++ pages/activity/vip/vip.vue
@@ -1,6 +1,11 @@
 <template>
 	<view class="vip-container">
-
+		<view class="vip">
+			<image src="../../../static/image/activity/vip.webp" mode=""></image>
+		</view>
+		<view class="vip">
+			<image src="../../../static/image/activity/ka.webp" mode=""></image>
+		</view>
 	</view>
 </template>
 
@@ -16,10 +21,15 @@
 	}
 </script>
 
-<style>
-	.vip.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
+<style lang="scss" scoped>
+	.vip-container {
+		.vip{
+			width: 100%;
+			height: 400rpx;
+			image{
+				width: 100%;
+				height: 100%;
+			}
+		}
 	}
 </style>
