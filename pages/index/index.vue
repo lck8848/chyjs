e00df62e15a4c44b975bf53da68dd322757ad533
shuoge <971749387@qq.com>
2020-10-16 23:08:49 +0800

    宫格

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -24,20 +24,45 @@
 					<view class="swiper-item">
 						<image src="@/static/image/swiper/swiper3.webp" mode=""></image>
 					</view>
-				</swiper-item>
+				</swiper-item>s
 			</swiper>
 		</view>
 		<!-- 轮播图 -->
 		
+		<!-- 六宫格 -->
 		
+		<view class="grid-container">
+			<view class="grid">
+				<view class="item" @click="Tosend">
+					<image src="@/static/images/index/grid1.jpg" mode=""></image>
+				</view>
+				<view class="item" @click="Tofruit">
+					<image src="@/static/images/index/grid2.jpg" mode=""></image>
+				</view>
+				<view class="item" @click="Tosnack">
+					<image src="@/static/images/index/grid3.jpg" mode=""></image>
+				</view>
+				<view class="item" @click="Totea">
+					<image src="@/static/images/index/grid4.jpg" mode=""></image>
+				</view>
+				<view class="item" @click="Towine">
+					<image src="@/static/images/index/grid5.jpg" mode=""></image>
+				</view>
+				<view class="item" @click="Toall">
+					<image src="@/static/images/index/grid6.jpg" mode=""></image>
+				</view>
+			</view>
+		</view>
+		
+		<!-- 六宫格  -->
 	</view>
 </template>
 
 <script>
-	import search from "../../components/search/search"
+	import search from "../../component/search/search"
 	export default {
 		components:{
-			search
+			search,
 		},
 		data() {
 			return {
@@ -47,7 +72,26 @@
 		methods: {
 			Tosearch(){
 				this.$refs.search.search()
-			}
+			},
+			Tosend(){
+				console.log("send")
+			},
+			Tofruit(){
+				console.log("fruit")
+			},
+			Tosnack(){
+				console.log("snack")
+			},
+			Totea(){
+				console.log("tea")
+			},
+			Towine(){
+				console.log("wine")
+			},
+			Toall(){
+				console.log("all")
+			},
+			
 		}
 	}
 </script>
@@ -71,6 +115,28 @@
 				}
 			}
 			
+		}
+		
+		.grid-container{
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-color: rgb(247, 246, 244);
+			padding: 20rpx;
+			.grid{
+				width: 700rpx;
+				display: flex;
+				flex-wrap: wrap;
+				justify-content: space-around;
+				.item{
+					width: 200rpx;
+					height: 200rpx;
+					image{
+						width: 100%;
+						height: 100%;
+					}
+				}
+			}
 		}
 	}
 </style>
