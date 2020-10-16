755f7f35457e3a24146f9bd3d7cc2f6c0dc6cd44
yao <54076703@qq.com>
2020-10-16 11:04:33 +0800

    four page

--- pages/activity/hot/hot.vue
+++ pages/activity/hot/hot.vue
@@ -1,6 +1,11 @@
 <template>
 	<view class="hot-container">
-	
+		<view class="hot">
+			<image src="../../../static/image/activity/hot.webp" mode=""></image>
+		</view>
+		<view class="hot">
+			<image src="../../../static/image/activity/ka.webp" mode=""></image>
+		</view>
 	</view>
 </template>
 
@@ -8,7 +13,7 @@
 	export default {
 		data() {
 			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+				
 			}
 		},
 		methods: {
@@ -17,10 +22,15 @@
 	}
 </script>
 
-<style>
-	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
+<style lang="scss" scoped>
+	.hot-container {
+		.hot{
+			width: 100%;
+			height: 400rpx;
+			image{
+				width: 100%;
+				height: 100%;
+			}
+		}
 	}
 </style>
--- pages/activity/monnew/monnew.vue
+++ pages/activity/monnew/monnew.vue
@@ -1,6 +1,11 @@
 <template>
 	<view class="monnew-container">
-		
+		<view class="monnew">
+			<image src="../../../static/image/activity/monnew.webp" mode=""></image>
+		</view>
+		<view class="monnew">
+			<image src="../../../static/image/activity/ka.webp" mode=""></image>
+		</view>
 	</view>
 </template>
 
@@ -17,10 +22,15 @@
 	}
 </script>
 
-<style>
-	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
+<style lang="scss" scoped>
+	.monnew-container {
+		.monnew{
+			width: 100%;
+			height: 400rpx;
+			image{
+				width: 100%;
+				height: 100%;
+			}
+		}
 	}
 </style>
