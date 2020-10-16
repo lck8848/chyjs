8e558dde5d6eff5d34e836efd8ee2830cef41ae2
shuoge <971749387@qq.com>
2020-10-16 09:25:37 +0800

    轮播图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,32 @@
 <template>
 	<view class="container">
+		<!-- 搜索框 -->
 		<view class="search-container" @click="Tosearch">
 			<search  ref="search"></search>
 		</view>
+		<!-- 搜索框 -->
 		
+		<!-- 轮播图 -->
+		<view class="swiper-container">
+			<swiper class="swiper" :indicator-dots="true" :autoplay="true" :interval="5000" :duration="500">
+				<swiper-item>
+					<view class="swiper-item">
+						<image src="../../static/image/swiper/swiper1.webp" mode=""></image>
+					</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item">
+						<image src="../../static/image/swiper/swiper2.webp" mode=""></image>
+					</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item">
+						<image src="../../static/image/swiper/swiper3.webp" mode=""></image>
+					</view>
+				</swiper-item>
+			</swiper>
+		</view>
+		<!-- 轮播图 -->
 		
 		
 	</view>
@@ -28,8 +51,25 @@
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
 	.container {
 		
+		.swiper-container{
+			width: 100%;
+			height: 370rpx;
+			.swiper{
+				width: 100%;
+				height: 100%;
+				.swiper-item{
+					width: 100%;
+					height: 100%;
+					image{
+						width: 100%;
+						height: 100%;
+					}
+				}
+			}
+			
+		}
 	}
 </style>
