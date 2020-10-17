91b8ec15bbe0373e7d7e01423dbf6f42693baeba
ztt <13434792579@163.com>
2020-10-17 11:24:36 +0800

    商品分类九宫格/回到顶部（首页）

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,7 +2,7 @@
 	<view class="container">
 		<!-- 搜索框 -->
 		<view class="search-container" @click="Tosearch">
-			<search  ref="search"></search>
+			<search ref="search"></search>
 		</view>
 		<!-- 轮播图 -->
 		<view class="swiper-container">
@@ -26,12 +26,12 @@
 			</swiper>
 		</view>
 
-		
+
 
 		<!-- 轮播图 -->
-		
+
 		<!-- 六宫格 -->
-		
+
 		<view class="grid-container">
 			<view class="grid">
 				<view class="item" @click="Tosend">
@@ -54,263 +54,426 @@
 				</view>
 			</view>
 		</view>
-		
+
 		<!-- 六宫格  -->
 		<!-- 种草笔记 -->
-				<view class="note_container">
-					<view class="top">
-						<text class="note">种草笔记</text>
-						<text class="more" @click="toMore">查看更多></text>
+		<view class="note_container">
+			<view class="top">
+				<text class="note">种草笔记</text>
+				<text class="more" @click="toMore">查看更多></text>
+			</view>
+			<view class="scroll-container">
+				<scroll-view class="scroll-view" :scroll-x="true" @scroll="scroll" :show-scrollbar="true">
+					<view id="demo1" class="item">
+						<view class="note_img">
+							<image src="../../static/image/note/luosifen-.jpg" class="luosifen"></image>
+						</view>
+						<view class="desc">
+							<text>
+								试吃了1个月、差点被开除，终于找到了内心满分的螺蛳粉！
+							</text>
+
+						</view>
 					</view>
-					<view class="scroll-container">
-						<scroll-view class="scroll-view" :scroll-x="true" @scroll="scroll"  :show-scrollbar="true" >
-							<view id="demo1" class="item">
-								<view class="note_img">
-									<image src="../../static/image/note/luosifen-.jpg" class="luosifen"></image>
-								</view>
-								<view class="desc">
-									<text>
-										试吃了1个月、差点被开除，终于找到了内心满分的螺蛳粉！
-									</text>
-									
-								</view>
-							</view>
-							<view id="demo1" class="item">
-								<view class="note_img">
-									<image src="../../static/image/note/luosifen-.jpg" class="luosifen"></image>
-								</view>
-								<view class="desc">
-									<text>
-										试吃了1个月、差点被开除，终于找到了内心满分的螺蛳粉！
-									</text>
-									
-								</view>
-							</view>
-							<view id="demo1" class="item">
-								<view class="note_img">
-									<image src="../../static/image/note/luosifen-.jpg" class="luosifen"></image>
-								</view>
-								<view class="desc">
-									<text>
-										试吃了1个月、差点被开除，终于找到了内心满分的螺蛳粉！
-									</text>
-									
-								</view>
-							</view>
-							
-		<!-- 				<view id="demo1" class="item">
-								<image src="../../static/image/note/dongzao.jpg" class="dongzao"></image>
-								<view class="desc">
-								
-									新疆，还有什么水果是你不行的？连枣都能甜哭我！
-								
-								</view>
-							</view>
-							<view id="demo1" class="item">
-								<image src="../../static/image/note/niurougan.jpg" class="niurougan"></image>
-								<view class="desc">
-								
-									什么样的牛肉干好吃？内蒙古的朋友有话说
-								
-								</view>
-							</view>
-							<view id="demo1" class="item">
-								<image src="../../static/image/note/jianguo.jpg" class="milk"></image>
-								<view class="desc">
-								
-									我找到了植物奶咖啡的平替（不是）
-								
-								</view>
-							</view>
-							<view id="demo1" class="item">
-								<image src="../../static/image/note/banfen.jpg" class="banfen"></image>
-								<view class="desc">
-								
-									吹爆南昌拌粉！一周有5天都想嗦它嗦它嗦它
-								
-								</view>
-							</view>
-							<view id="demo1" class="item">
-								<image src="../../static/image/note/guoba.jpg" class="guoba"></image>
-								<view class="desc">
-								
-									好吃的锅巴，会发出海鲜味的咔嚓咔嚓声
-								
-								</view>
-							</view>
-							<view id="demo1" class="item">
-								<image src="../../static/image/note/fengzhua.jpg" class="jizhua"></image>
-								<view class="desc">
-								
-									请把柠檬和鸡爪这对cp给我锁死死！
-								
-								</view>
-							</view>
-							<view id="demo1" class="item">
-								<image src="../../static/image/note/mifen.jpeg" class="mifen"></image>
-								<view class="desc">
-								
-									湖南人的乡愁，从一碗常德米粉开始
-								
-								</view>
-							</view> -->
-						</scroll-view>
+
+					<view id="demo1" class="item" @click="toDongzao">
+						<view class="note_img">
+							<image src="../../static/image/note/dongzao.jpg" class="dongzao" ></image>
+						</view>
+						<view class="desc">
+							<text>
+								新疆，还有什么水果是你不行的？连枣都能甜哭我！
+							</text>
+						</view>
 					</view>
+					<view id="demo1" class="item">
+						<view class="note_img">
+							<image src="../../static/image/note/niurougan.jpg" class="niurougan"></image>
+						</view>
+						<view class="desc">
+							<text>
+								什么样的牛肉干好吃？内蒙古的朋友有话说
+							</text>
+						</view>
+					</view>
+					<view id="demo1" class="item">
+						<view class="note_img">
+							<image src="../../static/image/note/jianguo.jpg" class="milk"></image>
+						</view>
+						<view class="desc">
+							<text>
+								我找到了植物奶咖啡的平替（不是）
+							</text>
+						</view>
+					</view>
+					<view id="demo1" class="item">
+						<view class="note_img">
+							<image src="../../static/image/note/banfen.jpg" class="banfen"></image>
+						</view>
+						<view class="desc">
+							<text>
+								吹爆南昌拌粉！一周有5天都想嗦它嗦它嗦它
+							</text>
+						</view>
+					</view>
+					<view id="demo1" class="item">
+						<view class="note_img">
+							<image src="../../static/image/note/guoba.jpg" class="guoba"></image>
+						</view>
+						<view class="desc">
+							<text>
+								好吃的锅巴，会发出海鲜味的咔嚓咔嚓声
+
+							</text>
+						</view>
+					</view>
+					<view id="demo1" class="item">
+						<view class="note_img">
+							<image src="../../static/image/note/fengzhua.jpg" class="jizhua"></image>
+						</view>
+						<view class="desc">
+							<text>
+								请把柠檬和鸡爪这对cp给我锁死死！
+							</text>
+						</view>
+					</view>
+					<view id="demo1" class="item">
+						<view class="note_img">
+							<image src="../../static/image/note/mifen.jpeg" class="mifen"></image>
+						</view>
+						<view class="desc">
+							<text>
+								湖南人的乡愁，从一碗常德米粉开始
+							</text>
+						</view>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
+		<!-- 分类宫格 -->
+		<view class="category_container">
+			<!-- 送ta礼物分类 -->
+			<view class="sendGift">
+				<image src="../../static/images/index/sendgift.webp" class="img"></image>
+				<view class="outside">
+						<view class="inside">
+										  <image src="../../static/images/index/crab.webp" class="small_item"></image>
+										  <text>［提蟹卡 阳澄湖大闸蟹］你的谢意 我们传达 顺丰包邮</text>
+										  <view class="money">
+										  	<text>￥298</text>
+											<image src="../../static/images/index/icon/cart-circle-o.png" class="cart"></image>
+										  </view>		 
+						</view>
+						
+						<view class="inside">
+										  <image src="../../static/images/index/crab.webp" class="small_item"></image>
+										  <text>［提蟹卡 阳澄湖大闸蟹］你的谢意 我们传达 顺丰包邮</text>
+										  <view class="money">
+										  	<text>￥298</text>
+											<image src="../../static/images/index/icon/cart-circle-o.png" class="cart"></image>
+										  </view>		 
+						</view>
+						
+						<view class="inside">
+										  <image src="../../static/images/index/crab.webp" class="small_item"></image>
+										  <text>［提蟹卡 阳澄湖大闸蟹］你的谢意 我们传达 顺丰包邮</text>
+										  <view class="money">
+										  	<text>￥298</text>
+											<image src="../../static/images/index/icon/cart-circle-o.png" class="cart"></image>
+										  </view>		 
+						</view>
+						
+						<view class="inside">
+										  <image src="../../static/images/index/crab.webp" class="small_item"></image>
+										  <text>［提蟹卡 阳澄湖大闸蟹］你的谢意 我们传达 顺丰包邮</text>
+										  <view class="money">
+										  	<text>￥298</text>
+											<image src="../../static/images/index/icon/cart-circle-o.png" class="cart"></image>
+										  </view>		 
+						</view>
+						
+						<view class="inside">
+										  <image src="../../static/images/index/crab.webp" class="small_item"></image>
+										  <text>［提蟹卡 阳澄湖大闸蟹］你的谢意 我们传达 顺丰包邮</text>
+										  <view class="money">
+										  	<text>￥298</text>
+											<image src="../../static/images/index/icon/cart-circle-o.png" class="cart"></image>
+										  </view>		 
+						</view>
+						
+						<view class="inside">
+										  <image src="../../static/images/index/crab.webp" class="small_item"></image>
+										  <text>［提蟹卡 阳澄湖大闸蟹］你的谢意 我们传达 顺丰包邮</text>
+										  <view class="money">
+										  	<text>￥298</text>
+											<image src="../../static/images/index/icon/cart-circle-o.png" class="cart"></image>
+										  </view>		 
+						</view>
 				</view>
+				
+			</view>
+		</view>
+		<!-- 分类宫格end -->
+		
+		<!-- 回到顶部 -->
+		<view class="top-button" @click="ToTop">
+			<image src="../../static/images/index/icon/top.png" class="topimg"></image>
+		</view>
 	</view>
 </template>
 
 <script>
-
 	import search from "../../component/search/search";
 
 	export default {
-		components:{
+		components: {
 			search,
 		},
 		data() {
 			return {
 				href: 'https://uniapp.dcloud.io/component/README?id=uniui',
 				scrollTop: 0,
-				old:{
+				old: {
 					scrollTop: 0
 				}
 			}
 		},
 		methods: {
-			Tosearch(){
+			Tosearch() {
 				this.$refs.search.search()
 			},
 
 			scroll: function(e) {
-			            console.log(e)
-			            this.old.scrollTop = e.detail.scrollTop
+				console.log(e)
+				this.old.scrollTop = e.detail.scrollTop
 			},
-			toMore(){
+			// 点击查看更多>(种草笔记)
+			toMore() {
 				uni.navigateTo({
-					url:"/pages/note/note"
+					url: "/pages/note/note"
 				})
 			},
 
-			Tosend(){
+			Tosend() {
 				console.log("send")
 			},
-			Tofruit(){
+			Tofruit() {
 				console.log("fruit")
 			},
-			Tosnack(){
+			Tosnack() {
 				console.log("snack")
 			},
-			Totea(){
+			Totea() {
 				console.log("tea")
 			},
-			Towine(){
+			Towine() {
 				console.log("wine")
 			},
-			Toall(){
+			Toall() {
 				console.log("all")
+			},
+			// 回到顶部
+			ToTop() {
+				uni.pageScrollTo({
+				scrollTop: 0,
+				duration: 300
+				})
 			}
-
-			
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
 	.container {
-		
-		.swiper-container{
+	background-color: #F9F9F9;
+	height:3000rpx;
+		.swiper-container {
 			width: 100%;
 			height: 370rpx;
-			.swiper{
+
+			.swiper {
 				width: 100%;
 				height: 100%;
-				.swiper-item{
+
+				.swiper-item {
 					width: 100%;
 					height: 100%;
-					image{
+
+					image {
 						width: 100%;
 						height: 100%;
 					}
 				}
 			}
-			
+
 		}
-		
+
 
 		// 种草笔记
-		
-		.note_container{
-			// background-color: #F9F9F9;
-			.top{
+
+		.note_container {
+
+			background-color: #F9F9F9;
+			.top {
 				display: flex;
 				justify-content: space-between;
-				.note{
-					font-size: 28rpx;
-					color:#323233;
+				align-items: center;
+				padding:0 20rpx;
+				.note {
+					font-size: 32rpx;
+					color: #323233;
 				}
-				.more{
-					font-size:24rpx;
-					color:#c7c7d1;
+
+				.more {
+					font-size: 24rpx;
+					color: #c7c7d1;
 				}
 			}
-			.scroll-container{
-				.scroll-view{
-					white-space:nowrap;
+
+			.scroll-container {
+				.scroll-view {
+					white-space: nowrap;
 					height: 450rpx;
 					width: 100%;
-					border:1px solid #E5E5E5;
+					margin-top:20rpx;
+					margin-left:20rpx;
+					// border: 1px solid #E5E5E5;
 					overflow: hidden;
-					.item{
+
+					.item {
 						// 这个属性可以使图片跟文字盒子上下排行
-						display:inline-block;
-						margin:20rpx;
+						display: inline-block;
 						background-color: white;
-						border:1px solid #E5E5E5;
-						.note_img{
+						border: 1px solid #E5E5E5;
+						.note_img {
 							width: 300rpx;
-							image{
+
+							image {
 								width: 100%;
 								height: 300rpx;
 							}
 						}
-						.desc{
+
+						.desc {
 							width: 300rpx;
-							height: 109rpx;
+							height: 100rpx;
+							padding:10rpx;
+							// 字体不换行
 							white-space: pre-wrap;
-							
-						 
-							text{
+
+							// 限制两行以后用省略号表示
+							text {
 								overflow: hidden;
 								text-overflow: ellipsis;
 								display: -webkit-box;
 								-webkit-box-orient: vertical;
 								-webkit-line-clamp: 2;
+								
+								font-weight: bold;
+								color:#4E4E4E
 							}
 						}
-						
+
 					}
 				}
 			}
-			
-		}
-		// 种草笔记到这
 
-		.grid-container{
+		}
+
+		// 种草笔记到这
+		
+		// 商品宫格分类
+		.category_container{
+			.sendGift{
+				.img{
+					width:100%;
+					height:180rpx;
+					
+				}
+				
+				width:750rpx;
+				// height:1000rpx;
+				.outside{
+					display: flex;
+					justify-content: space-around;
+					flex-wrap: wrap;
+					height:800rpx;
+					border:1px solid #F9F9F9;
+					.inside{
+						width:30%;
+						height:350rpx;
+						margin-bottom:10rpx;
+						background-color:white;
+						text{
+							padding:8rpx;
+							font-size:26rpx;
+							font-weight: bold;
+							
+							overflow: hidden;
+							text-overflow: ellipsis;
+							display: -webkit-box;
+							-webkit-box-orient: vertical;
+							-webkit-line-clamp: 2;
+						}
+						.small_item{
+							width:100%;
+							height:180rpx;
+						}
+						.money{
+							display: flex;
+							justify-content: space-between;
+							align-items: center;
+							padding:14rpx;
+							margin-left:-5rpx;
+							font-size:32rpx;
+							color:#FF4444;
+							.cart{
+								width:35rpx;
+								height:35rpx;
+							}
+						}
+					}
+									
+				}
+					
+				
+			}
+		}
+		// 商品宫格分类到这
+
+		// 回到顶部
+		.top-button {
+			width: 70upx;
+			height: 70upx;
+			// 固定定位
+			position: fixed;
+			right: 23upx;
+			bottom: 200upx;
+			z-index: 9999;
+			.topimg{
+				width:100rpx;
+				height:100rpx;
+			}
+		}
+		// 回到顶部到这
+		.grid-container {
 			display: flex;
 			justify-content: center;
 			align-items: center;
 			background-color: rgb(247, 246, 244);
 			padding: 10rpx;
-			.grid{
+
+			.grid {
 				width: 700rpx;
 				display: flex;
 				flex-wrap: wrap;
 				justify-content: space-around;
-				.item{
+
+				.item {
 					width: 200rpx;
 					height: 200rpx;
-					image{
+
+					image {
 						width: 100%;
 						height: 100%;
 					}
--- pages/note/note.vue
+++ pages/note/note.vue
@@ -1,49 +1,6 @@
 <template>
 	<view class="note_container">
-		<view class="title">
-			
-   新疆，还有什么水果是你不行的？连枣都能甜哭我！ 
-		</view>
-		<view class="time">
-			9月14日
-		</view>
-		<view class="content">
-			<view class="info">
-				大家都觉得所长是个嘴刁又贪吃的胖子，没错，我是，嘴刁能让我分辨美味，贪吃却让我边得简单，
-				比如<text>我吃了4颗新疆冬枣就开心得手足舞蹈对着屏幕傻傻的笑</text>
-				<image src="https://img.yzcdn.cn/upload_files/2020/09/14/23b4f9cd2cd859b7b33720aa186a9fbd.jpg" class="gif"></image>
-				<text>想要快乐，很简单呀，来吃新疆冬枣!</text>
-			</view>
-			<view class="info_bottom">
-				<text>个大皮薄，核小肉厚</text>
-				<text>浓浓枣味，ber脆ber甜的新疆冬枣</text>
-				<text>一颗一颗停不下来</text>
-			</view>
-			
-			<view class="first">
-				<h3>01</h3>
-				<view class="orange">
-					生活太苦，给你点甜
-				</view>
-				<text class="diagonal">/你离快乐只差一口新疆冬枣/</text>
-				<view class="info">
-					<text>红黄相间的新疆冬枣，光洗好摆在那里，就足够<text class="orange">赏心悦目秀色可餐</text>了。</text>
-					<image src="../../static//image//note/todongzao.jpg" class="img"></image>
-					<text>饱满圆润的枣，包裹的是努力长了一年的“肉肉,不仅<text class="orange">细腻紧实</text>，
-					关键还<text class="orange">肉厚核小</text>，一口一枣，放心大胆的啃食就完事了。</text>
-					<image src="../../static/image/note/todongzao2.jpg" mode=""></image>
-					<text>"咔嚓咔嚓",ber脆!两口吃掉一颗枣，越吃越上瘾</text>
-					<image src="../../static/image/note/todongzao3.jpg" mode=""></image>
-					<text>"很多人都误会新疆冬枣了，觉得新疆+冬枣=甜，贼甜，特别甜，当然啦，甜是不争的事实，我说一堆甜过初恋，甜到忧伤，都比不上真是测出来的糖度数</text>
-					<image src="../../static/image/note/measure.jpg" mode=""></image>
-					<text class="measure">真用九牛二虎之劲才挤出的糖水儿</text>
-					<text>但是，<text class="orange">新疆冬枣最让我心动的是它浓烈的早味</text>，甜味只是他复杂丰富味道中的一种，甚至在回味时才能清晰品味到他的甜蜜。
-					适度的酸味压制了傻白甜，带来3分清爽3分复杂4分浓郁
-					</text>
-					<image src="" mode=""></image>
-				</view>
-			</view>
-		</view>
+	
 	</view>
 </template>
 
