8fd6a78b7e358469b364c764f3b53d33faebfce0
youloby <1696279132@qq.com>
2020-10-19 08:35:52 +0800

    1

---
+++ pages/showcase/showcase.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		橱窗
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				bg_list: ['http://47.106.36.197:7000/source/other/ranking_bg.png',
+				'http://47.106.36.197:7000/source/other/recommend_bg.png',
+				'http://47.106.36.197:7000/source/other/footmark_bg.png']
+			}
+		},
+		methods: {
+			
+		},
+		onLoad: function (option) {
+			console.log(option.index);
+		}
+	}
+</script>
+
+<style>
+
+</style>
