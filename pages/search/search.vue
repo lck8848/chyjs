b2045e35b1165c97edd4fedefad70620c1033f1f
yao <54076703@qq.com>
2020-10-22 10:07:40 +0800

    修改充值页面bug

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -85,6 +85,7 @@
 				this.detail(val)
 			},
 			detail(key){
+				
 				console.log(key)
 				var data = key
 				this.history.push_unique(data)
@@ -140,6 +141,7 @@
 			}
 		},
 		onLoad(){
+			
 			Array.prototype.push_unique = function () {
 				for (var i = 0; i < arguments.length; i++) {
 					var ele = arguments[i];
@@ -151,6 +153,9 @@
 			this.history = JSON.parse(uni.getStorageSync('history'))
 		},
 		onShow() {
+			if(this.history == null){
+				return
+			}
 			this.history = JSON.parse(uni.getStorageSync('history'))
 			console.log(uni.getStorageSync('history'))
 			console.log(this.history)
